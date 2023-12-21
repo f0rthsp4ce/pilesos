10767594808b6f15716013fc47fb6fb1b4b25ffb
Mike_Went <git@meew.me>
2023-12-21 17:52:47 +0400

    add mocked gpio

--- pilesos/hardware/gpio.py
+++ pilesos/hardware/gpio.py
@@ -2,6 +2,21 @@
 Public GPIO interface. Usable from any module, simultaneously.
 """
 import pigpio
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+gpio: pigpio.pi
 
 # connect to pigpiod daemon
-gpio = pigpio.pi("pigpiod", port=8888)
+gpio = pigpio.pi("pigpiod", port=8888, show_errors=False)
+if not gpio.connected:
+    # virtual environment without gpio
+    from unittest.mock import MagicMock
+
+    gpio.set_mode = MagicMock()
+    gpio.write = MagicMock()
+    gpio.set_PWM_frequency = MagicMock()
+    gpio.set_PWM_dutycycle = MagicMock()
+
+    logger.warning("USING MOCKED GPIO. NO REAL PINS FOUND.")
