e35f0c2f2246bc45b131df689ee43579e0af2744
Mike_Went <git@meew.me>
2024-01-13 06:56:53 +0400

    add camera error handling

--- pilesos/hardware/camera.py
+++ pilesos/hardware/camera.py
@@ -1,4 +1,7 @@
+import logging
 import subprocess
+
+logger = logging.getLogger(__name__)
 
 
 class CameraController:
@@ -11,9 +14,12 @@ class CameraController:
 
     def apply_settings(self):
         for setting, value in self.settings.items():
-            subprocess.run(
-                ["v4l2-ctl", "-d", "/dev/video0", f"--set-ctrl={setting}={value}"]
+            p = subprocess.run(
+                ["v4l2-ctl", "-d", "/dev/video0", f"--set-ctrl={setting}={value}"],
+                capture_output=True,
             )
+            if p.stderr or p.stdout:
+                logger.error(f"{p.stdout=}, {p.stderr=}")
 
 
 front_camera = CameraController(
